cae1080fcb8782a2ce42308068973030928ecaad
sanella <grcicsanela89@gmail.com>
2016-02-13 14:48:26 +0100

    Product resource create

---
+++ db/migrate/20160213131036_create_products.rb
@@ -0,0 +1,10 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |tabele|
+      tabele.integer :price
+      tabele.string :name
+
+      tabele.timestamps null: false
+    end
+  end
+end
